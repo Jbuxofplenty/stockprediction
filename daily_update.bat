c47175f0d1e547eee601ffdb0cb5d70f62bba82b
jbuxofplenty <jbuxofplenty@gmail.com>
2018-01-24 20:07:21 -0700

    finished single stock daily update

---
+++ daily_update.bat
@@ -0,0 +1,4 @@
+stock_price_data.py
+prediction_machine.py
+log.py
+ECHO Stock Prediction Daily Update Completed
---
+++ log.py
@@ -0,0 +1,17 @@
+import datetime
+
+# Define today
+start_date = str(datetime.date.today())
+start_date.replace('/', '_')
+
+# Check to see if the log file has been run today
+with open('log.txt', mode='r') as file:
+    log = file.readline()
+    while(log):
+        if log.split()[0] == start_date:
+            quit()
+        log = file.readline()
+
+# Append the successful daily update to the log
+with open('log.txt', mode='a') as file:
+    file.write('%s - Stock Prediction Daily Update Completed' % (start_date))
